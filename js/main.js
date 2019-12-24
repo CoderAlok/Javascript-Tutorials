9f874d70869527fb9d8bb5b88a36670507b385ee
Alok <alokdas4all@gmail.com>
2019-12-25 01:41:25 +0530

    Added a js folder with all the js files

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// function to calculate teh Square of a number.
+function testSqr(a){
+	return a*a;
+}
+
+// This functions are related to the events with the elements.
+function Calculate(x){
+	alert("Result : "+testSqr(x));
+}
